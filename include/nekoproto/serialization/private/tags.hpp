389291fec6f6d299033f641dadb595f92a7d1718
llhsdmd <2982886437@qq.com>
2025-12-02 14:42:41 +0800

    [feature] Improve tags for reflection

--- include/nekoproto/serialization/private/tags.hpp
+++ include/nekoproto/serialization/private/tags.hpp
@@ -12,28 +12,79 @@
 
 #include "nekoproto/global/global.hpp"
 
-#include <string>
 #include <type_traits> // std::decay_t, std::is_same_v, std::remove_reference_t
 #include <utility>     // std::forward, std::move, std::remove_cvref_t
 
 NEKO_BEGIN_NAMESPACE
-struct Tags {
-    bool flat        = false;
-    bool skipable    = false;
-    bool fixedLength = false;
-    bool rawString   = false;
+struct NoTags {
+    template <typename T>
+        requires(std::is_class_v<T> && std::is_default_constructible_v<T> && std::is_aggregate_v<T> &&
+                 (!std::is_same_v<std::remove_cvref_t<T>, std::string_view>) &&
+                 (!std::is_same_v<std::remove_cvref_t<T>, std::string>))
+    operator T() const {
+        return T{};
+    }
 };
+
+struct JsonTags {
+    bool flat      = false;
+    bool skipable  = false;
+    bool rawString = false;
+};
+
+struct BinaryTags {
+    bool fixedLength = false;
+};
+
+namespace tag_access {
+template <typename T>
+constexpr bool is_flat(const T& tags) {
+    if constexpr (requires { tags.flat; }) {
+        return tags.flat;
+    } else {
+        return false;
+    }
+}
+
+template <typename T>
+constexpr bool is_skipable(const T& tags) {
+    if constexpr (requires { tags.skipable; }) {
+        return tags.skipable;
+    } else {
+        return false;
+    }
+}
+
+template <typename T>
+constexpr bool is_fixed_length(const T& tags) {
+    if constexpr (requires { tags.fixedLength; }) {
+        return tags.fixedLength;
+    } else {
+        return false;
+    }
+}
+
+template <typename T>
+constexpr bool is_raw_string(const T& tags) {
+    if constexpr (requires { tags.rawString; }) {
+        return tags.rawString;
+    } else {
+        return false;
+    }
+}
+} // namespace tag_access
 
 // --- The Core Wrapper Template ---
 template <auto Tags, typename InnerValue>
 struct TaggedValue {
     using raw_type             = InnerValue;
-    constexpr static auto tags = Tags;
+    using tag_type             = decltype(Tags); // 获取 Tag 的实际类型
+    constexpr static auto tags = Tags;           // NOLINT
     InnerValue value;
 };
 
 template <auto Tags, typename InnerValue>
-inline constexpr auto make_tags(InnerValue&& value) {
+inline constexpr auto make_tags(InnerValue&& value) { // NOLINT
     return TaggedValue<Tags, InnerValue>{std::forward<InnerValue>(value)};
 }
 
@@ -49,19 +100,24 @@ struct is_tagged_value<const TaggedValue<Tags, Inner>> : std::true_type {};
 template <auto Tags, typename Inner>
 struct is_tagged_value<const TaggedValue<Tags, Inner>&> : std::true_type {};
 template <typename T>
-inline constexpr bool is_tagged_value_v = is_tagged_value<std::remove_cvref_t<T>>::value; // NOLINT
+inline constexpr bool is_tagged_value_v = is_tagged_value<std::decay_t<T>>::value; // NOLINT
 
 template <typename T, class enable = void>
-struct unwrap_tags {
-    using type = T;
+struct unwrap_tags { // NOLINT
+    using type                 = T;
+    constexpr static auto tags = NoTags{}; // NOLINT
 };
 
 template <typename T>
 struct unwrap_tags<T, std::enable_if_t<is_tagged_value_v<T>>> {
-    using type = typename T::raw_type;
+    using type                 = typename std::decay_t<T>::raw_type;
+    constexpr static auto tags = std::decay_t<T>::tags; // NOLINT
 };
 
 template <typename T>
 using unwrap_tags_t = typename unwrap_tags<T>::type;
+
+template <typename T>
+inline constexpr auto unwrap_tags_v = unwrap_tags<T>::tags; // NOLINT
 
 NEKO_END_NAMESPACE
